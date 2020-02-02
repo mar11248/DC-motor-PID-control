d37659bd3c7b7477b4dec807974561a8a33aeda1
mar11248 <50755298+mar11248@users.noreply.github.com>
2020-02-01 19:35:30 -0600

    Update main.ino
    Speed control commented, and instead position control added. Still need to implement hardware input to choose the control mode and input values.

--- main/main.ino
+++ main/main.ino
@@ -13,21 +13,26 @@ int M2 = 4; //to enable pin A2 of motor driver
 double pulse = 0;
 double abs_pulse = 0; //saves how many pulses did the encoder send
 
-double PM =0;
+double PM = 0;
 double RPM = 0;
-double old_time=0;
-double new_time=0;
-double new_position=0;
-double old_position=0;
+double RPMmot = 0;
+double old_time = 0;
+double new_time = 0;
+double new_position = 0;
+double old_position = 0;
 
 boolean Read_Dir; //Saves the direction of the motor
 int Set_Dir; //0=stop motor, 1=right motor, 2=left motor
 
-double pwm; //pulse sent to the motor
+double pwm = 20; //pulse sent to the motor
 double setpoint; //desired number for the PID control
 
-double kp = 1, ki = 1, kd = 0; //constants for the PID control
-PID motor(&abs_pulse, &pwm, &setpoint, kp, ki, kd, DIRECT);
+double angle = 0;
+
+//double kp = 0.1, ki = 0.1, kd = 0.015; //constants for the PID control
+double kp = 0.1, ki = 0.1, kd = 0.2; //constants for the PID control
+//PID motor(&RPM, &pwm, &setpoint, kp, ki, kd, DIRECT);
+PID motor(&pulse, &pwm, &setpoint, kp, ki, kd, DIRECT);
 
 void read_encoder1() {
   // look for a low-to-high on channel A
@@ -52,7 +57,7 @@ void read_encoder1() {
       pulse--;          // CCW
     }
   }
-  
+
 }
 
 
@@ -82,33 +87,75 @@ void read_encoder2() {
   //Serial.println (pulse, DEC);
 }
 void setup() {
+  //TCCR1B = TCCR1B & 0b11111000 | 1;  // set 31KHz PWM to prevent motor noise
   Serial.begin(250000); //init the serial port
-  setpoint = 20; //set the power
+  setpoint = map (90, 0, 360, 0, 420); // mapping degree into pulse
+  
   pinMode(M1, OUTPUT);
   pinMode(M2, OUTPUT);
 
   pinMode(encoder_A, INPUT);
   pinMode(encoder_B, INPUT);
 
+  motor.SetMode(AUTOMATIC);
+  motor.SetOutputLimits(-80, 80);
+  motor.SetSampleTime(100);
+
   attachInterrupt(0, read_encoder1, CHANGE);
 
- attachInterrupt(1, read_encoder2, CHANGE);
+  // attachInterrupt(1, read_encoder2, CHANGE);
 
 
 }
 
 void loop() {
-  digitalWrite(M2, LOW);
-  analogWrite(M1, 70);
-  new_position=abs(pulse);
+  motor.Compute();
+  if (pulse>setpoint) {
+
+    advance((int)pwm);
+
+    Serial.print(pwm);
+
+  }
+  else  {
+
+    int pwm1 = abs(pwm);
+    back(pwm1);
+
+    Serial.print(pwm);
+
+  }
+
+
+
+  new_position = abs(pulse);
   new_time = micros();
-  PM=((new_position-old_position)/((new_time-old_time)*0.000001))*60;
-  RPM=(PM/420)/30;
-  old_position=new_position;
+  PM = ((new_position - old_position) / ((new_time - old_time) * 0.000001)) * 60;
+  RPM = abs(PM / 420);
+  RPMmot = RPM / 30;
+  angle = new_position * 0.8571428571428;
+  old_position = new_position;
   old_time = new_time;
-  
-  Serial.println(RPM);
-  delay(100);
+
+  Serial.print(",");
+  Serial.println(angle);
+  //delay(100);
 
 
+}
+void advance(int pwme)//Motor Forward
+{
+  digitalWrite(M2, LOW);
+  analogWrite(M1, pwme);
+}
+void back(int pwme)//Motor reverse
+{
+  digitalWrite(M2, HIGH);
+  analogWrite(M1, pwme);
+}
+
+void Stop()//Motor stops
+{
+  digitalWrite(M1, LOW);
+  digitalWrite(M2, LOW);
 }
