75ee0c9e6299de81643c651bd92eebfc9e5643d4
mar11248 <50755298+mar11248@users.noreply.github.com>
2020-02-14 19:15:37 -0600

    working version for both controls
    several variables unused erased,  added control with nunchuck

--- main/main.ino
+++ main/main.ino
@@ -2,60 +2,60 @@
 // Autor: Adrián Martínez Girón
 // for Universidad del Valle de Guatemala
 
-//#define DEBUG
 #include <PID_v1.h>
+#include "nunchuk.h"
 
-#define SAMPLE_DELAY (5)   //  this gets 1 reading per second.
+
+#define SAMPLE_DELAY (20)   //  this gets 1 reading per X milliseconds.
 //  adjust the delay to fit your needs
-#define PULSES_PER_TURN (420)  //  32 state changes per turn on 1 line, 
+#define PULSES_PER_TURN (1311)  //  1311 state changes per turn on 1 line, 
+
+int x, y;
+int b1, b2;
+
+double angle=0;
 
 unsigned int lastTime; //Use for speed
 
 boolean mode = true; //change to false for speed control
-int cont = 0;
 const byte encoder_A = 2; // to interrupt 0
 const byte encoder_B = 3; // to digital pin 3
 
-int M1 = 5; //to enable pin A1 of motor driver
-int M2 = 4; //to enable pin A2 of motor driver
+uint8_t M1 = 5; //to enable pin A1 of motor driver
+uint8_t M2 = 4; //to enable pin A2 of motor driver
 
-double pulse = 0;
-double abs_pulse = 0; // saves how many pulses did the encoder send
+uint16_t input = 0;
 
-double PM = 0;        // pulses per minute
+volatile double pulse = 0;
+volatile uint16_t abs_pulse = 0; // saves how many pulses did the encoder send
+
 double RPM = 0;       // revolutions per minute
 
-double old_time = 0;         // get time differential
-double new_time = 0;
-double new_position = 0;  // get position differential
-double old_position = 0;
-
-String inString = "";
-int incomingByte = 0; //get byte from serial
-int input = 0;  // serial number saved
+float new_position = 0;  // get position differential
 
 double pwm = 0; //pulse sent to the motor
 double setpoint; //desired number for the PID control
-double angle = 0; // motor position in DEG
 
 double kp1 = 0.16617, ki1 = 1.3085, kd1 = 0; //constants for the PID Speed control
-double kp = 0.3, ki = 0.4, kd = 0.1; //constants for the PID Position control
-
+double kp = 0.1, ki = 0.7 , kd = 0; //constants for the PID Position control
 PID motor(&pulse, &pwm, &setpoint, kp, ki, kd, DIRECT);   // PID for the position
 PID motor1(&RPM, &pwm, &setpoint, kp1, ki1, kd1, DIRECT); // PID for the speed
 
 void setup() {
+  Wire.begin();
+  nunchuk_init();
   if (mode) {
-    motor.SetOutputLimits(20, 250);
+    motor.SetOutputLimits(0, 200);
   }
   if (!mode) {
     motor1.SetOutputLimits(0, 255);
   }
-  pinMode(A0, INPUT);
-  pinMode(A1, INPUT);
-  pinMode(A2, INPUT);
+  pinMode(A0, INPUT); // Control potentiometer input
+
+  pinMode(10, INPUT_PULLUP); // Mode change input
+
   TCCR1B = TCCR1B & 0b11111000 | 1;  // set 31KHz PWM to prevent motor noise
-  Serial.begin(250000); //init the serial port
+
 
   pinMode(M1, OUTPUT); // set output pins for motor control
   pinMode(M2, OUTPUT);
@@ -63,83 +63,84 @@ void setup() {
   pinMode(encoder_A, INPUT);  // set encoder inputs
   pinMode(encoder_B, INPUT);
 
-  motor.SetMode(AUTOMATIC);
+  motor.SetMode(AUTOMATIC);  // Settings for the PID controllers
   motor1.SetMode(AUTOMATIC);
   motor.SetSampleTime(100);
   motor1.SetSampleTime(100);
 
-  attachInterrupt(0, read_encoder1, CHANGE);   // one interrupt enabled, if we use both we can
-  //attachInterrupt(1, read_encoder2, CHANGE); // get the full encoder resolution, instead of
+  attachInterrupt(0, doEncoderA, CHANGE);   // one interrupt enabled, if we use both we can
+  attachInterrupt(1, doEncoderB, CHANGE); // get the full encoder resolution, instead of
   // half the resolution.
 }
 
 void loop() {
-  // Read the input from serial
-  while (Serial.available() > 0) {
-    int inChar = Serial.read();
-    if (isDigit(inChar)) {
-      // convert the incoming byte to a char and add it to the string:
-      inString += (char)inChar;
-    }
-    // if you get a newline, print the string, then the string's value:
-    if (inChar == '\n') {
-      input = inString.toInt();
-      // clear the string for new input:
-      inString = "";
-    }
+  if (nunchuk_read()) {
+    // Work with nunchuk_data
+    x = nunchuk_joystickX();
+    y = nunchuk_joystickY();
+    b1 = nunchuk_buttonZ();
+    b2 = nunchuk_buttonC();
+
   }
+  if (b1 == 1) {
+    mode = !mode;
+    delay(250);
+  }
+
+
   // ----------- Position control -----------
   if (mode) {
-    setpoint = mapfloat(input, 0, 360, 0, 420);
+
+    // setpoint = mapfloat(input, 0, 1024, 0, PULSES_PER_TURN);
+    angle=atan(x/y); 
+    if(((x>-40)&&(x<40))||((y>-40)&&(y<40))){
+      setpoint=setpoint;
+    }
+    else{
+          setpoint = mapfloat(angle, 0, 6.28319, 0, PULSES_PER_TURN);
+
+    }
+    
+    motor.Compute();
+
     if (pulse < (setpoint - 1)) {
-      motor.Compute();
       motor.SetControllerDirection(DIRECT);
-      advance((int)pwm);
+      back((int)pwm);
     }
     else if (pulse > (setpoint + 1)) {
-      motor.Compute();
       int pwm1 = abs(pwm);
       motor.SetControllerDirection(REVERSE);
-      back((int)pwm1);
+      advance((int)pwm1);
     } else {
       Stop();
     }
-    noInterrupts(); // disable interrutps between reads to avoid reading while the value change
+    noInterrupts(); // disable interrutps between reads to avoid reading
+    // while the value change
     new_position = abs(pulse);
     interrupts(); // enable interrupts and continue
-    angle = map (new_position, 0, 420, 0, 360);
+
+
   }
   // ----------- Speed Control -----------
   if (!mode) {
-    setpoint = input;
+    input = analogRead(A0);
+    // setpoint = map(input, 0, 1024, 0, 500);
+    setpoint = map(y, 10, 90, 0, 500);
     motor1.Compute();
     advance(pwm);
-    
-    if ((unsigned int)millis() - lastTime >= SAMPLE_DELAY)
+
+    if (((unsigned int)millis() - lastTime) >= SAMPLE_DELAY)
     {
-      RPM = (abs(pulse) * (60000.f / ((unsigned int)millis() - lastTime))) / PULSES_PER_TURN;
+      noInterrupts(); // disable interrutps between reads to avoid reading
+      // while the value change
+      int abs_pulse = abs(pulse);
+      interrupts(); // enable interrupts and continue
+      RPM = (abs_pulse * (60000.f / ((unsigned int)millis() - lastTime))) / PULSES_PER_TURN;
       pulse = 0;
       lastTime = (unsigned int)millis();
     }
-// debug routine for tunning PID control in speed
-#ifdef DEBUG
-    double P = analogRead(A2);
-    double I = analogRead(A1);
-    double D = analogRead(A0);
-    kp1 = mapfloat(P, 0, 1024, 0.01, 20);
-    ki1 = mapfloat(I, 0, 1024, 0, 20);
-    kd1 = mapfloat(D, 0, 1024, 0, 1);
-    motor1.SetTunings(kp1, ki1, kd1);
-
-    Serial.print(kp1, DEC);
-    Serial.print(",");
-    Serial.print(ki1, DEC);
-    Serial.print(",");
-    Serial.print(kd1, DEC);
-    Serial.print(",");
-    Serial.println(RPM);
-#endif
   }
+
 }
 
 // Motor direction routines
@@ -161,17 +162,60 @@ void Stop()//Motor stops
 }
 
 //our map function for float
-double mapfloat(double x, double in_min, double in_max, double out_min, double out_max)
+float mapfloat(float x, float in_min, float in_max, float out_min, float out_max)
 {
-  return (double)(x - in_min) * (out_max - out_min) / (double)(in_max - in_min) + out_min;
+  return (float)(x - in_min) * (out_max - out_min) / (float)(in_max - in_min) + out_min;
 }
 
-// encoder reading routine
-void read_encoder1() {
+void doEncoderA() {
   // look for a low-to-high on channel A
-  if (digitalRead(encoder_A) == digitalRead(encoder_B)) {
-    pulse++;
-  } else {
-    pulse--;
+  if (digitalRead(encoder_A) == HIGH) {
+
+    // check channel B to see which way encoder is turning
+    if (digitalRead(encoder_B) == LOW) {
+      pulse = pulse + 1;         // CW
+    }
+    else {
+      pulse = pulse - 1;         // CCW
+    }
+  }
+
+  else   // must be a high-to-low edge on channel A
+  {
+    // check channel B to see which way encoder is turning
+    if (digitalRead(encoder_B) == HIGH) {
+      pulse = pulse + 1;          // CW
+    }
+    else {
+      pulse = pulse - 1;          // CCW
+    }
+  }
+
+  // use for debugging - remember to comment out
+}
+
+void doEncoderB() {
+  // look for a low-to-high on channel B
+  if (digitalRead(encoder_B) == HIGH) {
+
+    // check channel A to see which way encoder is turning
+    if (digitalRead(encoder_A) == HIGH) {
+      pulse = pulse + 1;         // CW
+    }
+    else {
+      pulse = pulse - 1;         // CCW
+    }
+  }
+
+  // Look for a high-to-low on channel B
+
+  else {
+    // check channel B to see which way encoder is turning
+    if (digitalRead(encoder_A) == LOW) {
+      pulse = pulse + 1;          // CW
+    }
+    else {
+      pulse = pulse - 1;          // CCW
+    }
   }
 }
